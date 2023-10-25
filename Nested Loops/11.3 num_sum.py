8381646c57d350b021531fb94fd955c783e8974b
Kalin Krumov <44120339+kaweeo@users.noreply.github.com>
2023-10-25 12:16:17 +0300

    Delete Nested Loops directory

--- Nested Loops/11.3 num_sum.py
+++
@@ -1,9 +0,0 @@
-
-total = int(input())
-sum_num = 0
-while True:
-    number = int(input())
-    sum_num += number
-    if sum_num >= total:
-        break
-print(sum_num)
--- Nested Loops/11.4 seq_2k_plus_1.py
+++
@@ -1,9 +0,0 @@
-
-n = int(input())
-k = 1
-
-while k <= n:
-    print(k)
-    k = 2 * k + 1
-
-
