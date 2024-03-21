4f303483ee2a7dec67be7f14018ff3070f724bca
Kalin Krumov <kaweeo@gmail.com>
2024-03-21 09:53:03 +0200

    Add more solutions

---
+++ Python OOP Feb 2024/SOLID /after/emails.py
@@ -0,0 +1,78 @@
+from abc import abstractmethod, ABC
+
+
+class IContent(ABC):
+
+    def __init__(self, text: str):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        ...
+
+
+class MyMl(IContent):
+
+    def format(self):
+        return '\n'.join(['<myML>', self.text, '</myML>'])
+
+
+class HTML(IContent):
+
+    def format(self):
+        return '\n'.join(['<html>', self.text, '</html>'])
+
+
+class IEmail(ABC):
+
+    @abstractmethod
+    def set_sender(self, sender):
+        pass
+
+    @abstractmethod
+    def set_receiver(self, receiver):
+        pass
+
+    @abstractmethod
+    def set_content(self, content):
+        pass
+
+
+class Email(IEmail):
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.__sender = None
+        self.__receiver = None
+        self.__content = None
+
+    def set_sender(self, sender):
+        # TODO: fix sender
+        if self.protocol == 'IM':
+            self.__sender = ''.join(["I'm ", sender])
+        else:
+            self.__sender = sender
+
+    def set_receiver(self, receiver):
+        # TODO: fix receiver
+        if self.protocol == 'IM':
+            self.__receiver = ''.join(["I'm ", receiver])
+        else:
+            self.__receiver = receiver
+
+    def set_content(self, content: IContent):
+        self.__content = content.format()
+
+    def __repr__(self):
+
+        template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
+
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
+
+
+email = Email('IM')
+email.set_sender('qmal')
+email.set_receiver('james')
+content = HTML('Hello, there!')
+email.set_content(content)
+print(email)
