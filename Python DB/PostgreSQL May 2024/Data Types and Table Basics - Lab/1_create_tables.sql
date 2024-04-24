340c9b11748d505325073aed6c92242e42baaa14
Kalin Krumov <kaweeo@gmail.com>
2024-04-24 11:32:31 +0300

    Add solutions

--- Python DB/PostgreSQL May 2024/Data Types and Table Basics - Lab/1_create_tables.sql
+++ Python DB/PostgreSQL May 2024/Data Types and Table Basics - Lab/1_create_tables.sql
@@ -6,3 +6,20 @@ CREATE TABLE employees (
     salary NUMERIC(10, 2),
     devices_number INT2
 );
+
+
+CREATE TABLE departments (
+    id serial PRIMARY KEY,
+    name VARCHAR(50),
+    code CHAR(3),
+    description TEXT
+);
+
+CREATE TABLE issues (
+	id serial PRIMARY KEY,
+	description VARCHAR(150),
+	date DATE,
+	"start" "timestamp"
+);
+
+
