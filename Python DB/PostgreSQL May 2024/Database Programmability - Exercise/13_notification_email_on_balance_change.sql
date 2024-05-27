7b04b018f6ebda0457aa98f40183ca23c64ccee6
Kalin Krumov <kaweeo@gmail.com>
2024-05-27 16:55:03 +0300

    Add more solutions

---
+++ Python DB/PostgreSQL May 2024/Database Programmability - Exercise/13_notification_email_on_balance_change.sql
@@ -0,0 +1,28 @@
+CREATE TABLE notification_emails
+(
+    id           SERIAL PRIMARY KEY,
+    recipient_id INT,
+    subject      VARCHAR(100),
+    body         TEXT
+);
+
+CREATE OR REPLACE FUNCTION trigger_fn_send_email_on_balance_change()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    INSERT INTO notification_emails (recipient_id, subject, body)
+    VALUES (
+        NEW.account_id,
+        concat('Balance change for account:', ' ', NEW.account_id),
+        concat_ws(' ', 'On', NOW()::date, 'your balance was changed from', NEW.old_sum,'to', NEW.new_sum,'.')
+        );
+    RETURN NEW;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tr_send_email_on_balance_change
+    AFTER UPDATE
+    ON logs
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_fn_send_email_on_balance_change();
