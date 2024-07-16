a6ba4f6a2b1f1db2a8ce690e90d0d2708aa6236e
Kalin Krumov <kaweeo@gmail.com>
2024-07-16 12:30:46 +0300

    Add solutions

---
+++ Python DB/Python ORM June 2024/10_Django_Models_Relations _lab/main_app/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from datetime import date
+
+
+# 01. The Lecturer
+class Lecturer(models.Model):
+    first_name = models.CharField(
+        max_length=100,
+    )
+    last_name = models.CharField(
+        max_length=100,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Subject(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    code = models.CharField(
+        max_length=10,
+    )
+    lecturer = models.ForeignKey(
+        to=Lecturer,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='subjects'
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+# 02. The Student
+class Student(models.Model):
+    student_id = models.CharField(
+        max_length=10,
+        primary_key=True,
+    )
+    first_name = models.CharField(
+        max_length=100,
+    )
+    last_name = models.CharField(
+        max_length=100,
+    )
+    birth_date = models.DateField()
+    email = models.EmailField(
+        unique=True
+    )
+    subjects = models.ManyToManyField(to=Subject, through='StudentEnrollment')
+
+
+# 03. The Enrollment
+
+class StudentEnrollment(models.Model):
+    class Grade(models.TextChoices):
+        A = 'A'
+        B = 'B'
+        C = 'C'
+        D = 'D'
+        F = 'F'
+
+    student = models.ForeignKey(
+        to=Student,
+        on_delete=models.CASCADE,
+    )
+    subject = models.ForeignKey(
+        to=Subject,
+        on_delete=models.CASCADE,
+    )
+    enrollment_date = models.DateField(
+        default=date.today
+    )
+    grade = models.CharField(
+        max_length=1,
+        choices=Grade.choices
+    )
+
+
+# 04. The Lecturer Profile
+
+class LecturerProfile(models.Model):
+    lecturer = models.OneToOneField(
+        to=Lecturer,
+        on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True, blank=True)
+    office_location = models.CharField(max_length=100, null=True, blank=True)
