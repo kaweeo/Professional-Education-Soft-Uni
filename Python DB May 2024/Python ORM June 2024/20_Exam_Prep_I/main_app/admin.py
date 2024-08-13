2761634644b0d7f2544bc31a8dbc53da7b9f4676
Kalin Krumov <kaweeo@gmail.com>
2024-08-13 08:42:34 +0300

    Add more solutions

---
+++ Python DB May 2024/Python ORM June 2024/20_Exam_Prep_I/main_app/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+from main_app.models import Director, Actor, Movie
+from managers import DirectorManager
+
+
+# Register your models here.
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date', 'nationality')
+    list_filter = ('years_of_experience',)
+    search_fields = ('full_name', 'nationality')
+    search_help_text = 'Search by Director full name or Nationality'
+
+    objects = DirectorManager()
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'birth_date', 'nationality']
+    list_filter = ('is_awarded',)
+    search_fields = ('full_name',)
+    readonly_fields = ('last_updated',)
+    search_help_text = 'Stella is super cozy'
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'storyline', 'rating', 'director')
+    list_filter = ('is_awarded', 'is_classic', 'genre')
+    search_fields = ('title', 'director__full_name')
+    readonly_fields = ('last_updated',)
+    search_help_text = 'Search by Director full name or title'
---
+++ Python DB May 2024/Python ORM June 2024/20_Exam_Prep_I/managers.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models import QuerySet, Count
+
+
+class DirectorManager(models.Manager):
+    def get_directors_by_movies_count(self) -> QuerySet:
+        return (
+            self
+            .prefetch_related('movies')
+            .annotate(movies_count=Count('movies'))
+            .order_by('-movies_count', 'full_name')
+        )
+
+
