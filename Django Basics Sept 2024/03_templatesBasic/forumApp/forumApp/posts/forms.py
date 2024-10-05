77b197e5d085d4d9273f017a43fa3e29dd97bbd7
Kalin Krumov <kaweeo@gmail.com>
2024-10-05 21:12:14 +0300

    Fix: added STATIC_ROOT setting

--- Django Basics Sept 2024/03_templatesBasic/forumApp/forumApp/posts/forms.py
+++ Django Basics Sept 2024/03_templatesBasic/forumApp/forumApp/posts/forms.py
@@ -1,9 +1,26 @@
 from django import forms
 
 class PersonForm(forms.Form):
+    STATUS_CHOICES = (
+        (1, 'Draft'),
+        (2, 'Published'),
+        (3, 'Archived'),
+    )
+
     person_name = forms.CharField(
         # widget=forms.TextInput(
         #     attrs={'class': 'form-control'}
         # )
     )
+
     age = forms.IntegerField()
+
+    is_lecturer = forms.BooleanField()
+
+    status = forms.IntegerField(
+        widget=forms.Select(choices=STATUS_CHOICES)
+    )       # IntegerField + widget returns ints, ChoiceField will return strs
+
+    # status = forms.ChoiceField(
+    #     choices=STATUS_CHOICES,
+    # )
